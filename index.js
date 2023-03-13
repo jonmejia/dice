e76c46b22261ec1dc0a80a045d119e1b05a800ac
Jon <jonathan@dataminded.net>
2023-03-12 23:27:11 -0400

    added a function to roll right die

--- index.js
+++ index.js
@@ -17,4 +17,19 @@ function rollLeftDie() {
     document.getElementById("img1").setAttribute("src", "images/dice5.png");
   }
 }
-rollLeftDie;
+function rollRightDie() {
+  dieValue = getRandomNumber();
+  if (dieValue === 1) {
+    document.getElementById("img2").setAttribute("src", "images/dice1.png");
+  } else if (dieValue === 2) {
+    document.getElementById("img2").setAttribute("src", "images/dice2.png");
+  } else if (dieValue === 3) {
+    document.getElementById("img2").setAttribute("src", "images/dice3.png");
+  } else if (dieValue === 4) {
+    document.getElementById("img2").setAttribute("src", "images/dice4.png");
+  } else if (dieValue === 5) {
+    document.getElementById("img2").setAttribute("src", "images/dice5.png");
+  }
+}
+rollLeftDie();
+rollRightDie();
