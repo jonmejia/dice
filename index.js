24ff9cbdd95913a75c518ee496b48baca2d19448
Jon <jonathan@dataminded.net>
2023-03-12 23:24:58 -0400

    chnaged the img class to an id, and added a function to roll the left die

---
+++ index.js
@@ -0,0 +1,20 @@
+function getRandomNumber() {
+  let randomNumber1 = Math.floor(Math.random() * 6);
+  return randomNumber1;
+}
+
+function rollLeftDie() {
+  dieValue = getRandomNumber();
+  if (dieValue === 1) {
+    document.getElementById("img1").setAttribute("src", "images/dice1.png");
+  } else if (dieValue === 2) {
+    document.getElementById("img1").setAttribute("src", "images/dice2.png");
+  } else if (dieValue === 3) {
+    document.getElementById("img1").setAttribute("src", "images/dice3.png");
+  } else if (dieValue === 4) {
+    document.getElementById("img1").setAttribute("src", "images/dice4.png");
+  } else if (dieValue === 5) {
+    document.getElementById("img1").setAttribute("src", "images/dice5.png");
+  }
+}
+rollLeftDie;
